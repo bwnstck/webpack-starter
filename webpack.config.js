06c10ed8e349d9325d86276e7a367532fea62358
bwnstk <info@flimmerkiste.xyz>
2020-10-19 16:31:30 +0200

    Add Output Management in app.bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    app: './src/index.js',
+  },
   output: {
-    filename: "bundle.js",
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, "dist"),
   },
   devtool: "inline-source-map",
