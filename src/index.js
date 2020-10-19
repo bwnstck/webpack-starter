f4f7f94e551d45ca3ee70c17a313a62b6f20269f
bwnstk <info@flimmerkiste.xyz>
2020-10-19 16:15:48 +0200

    Add Css and Images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import "./style.css";
+import Star from "./star.png";
 
 function component() {
   const element = document.createElement("div");
@@ -7,6 +8,12 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  // Add the image to our existing div.
+  const myStar = new Image();
+  myStar.src = Star;
+  element.append(myStar);
+
   return element;
 }
 
