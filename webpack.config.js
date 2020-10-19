8a050b00c1c451d84a2f2057e8216d581c573b79
bwnstk <info@flimmerkiste.xyz>
2020-10-19 17:05:59 +0200

    Add Dist-Cleaner

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
-    app: './src/index.js',
+    app: "./src/index.js",
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: '[name].bundle.js',
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   devtool: "inline-source-map",
