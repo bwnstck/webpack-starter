8e018d6889634d9e75d95283ee3ebdf944c593ce
bwnstk <info@flimmerkiste.xyz>
2020-10-20 10:08:08 +0200

    Inital WebPack Commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import _ from "lodash";
 import "./style.css";
-import Star from "./star.png";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  // Add the image to our existing div.
-  const myStar = new Image();
-  myStar.src = Star;
-  element.append(myStar);
+  element.innerHTML = "Moin";
 
   return element;
 }
